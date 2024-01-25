b01a43904a3ac1dac561b07c2e1df656b4e639b8
mohammb04 <mohammb@hiof.no>
2024-01-26 00:56:05 +0100

    Legodudes nettbutikk

---
+++ shopscript.js
@@ -0,0 +1,12 @@
+let productsHTML= "<h2>NinjaGO</h2>"
+
+products.map(product => productsHTML += `<article>
+<img src="website_images/PROD_${product.imagefile}" alt="PRODUKTBILDE"/>
+<a href="#">${product.category}</a>
+<h3>${product.title}</h3>
+<span>Kr. ${product.price}</span>
+<button>Legg i handlekurv</button>
+</article>`)
+
+const main = document.getElementsByTagName("main")
+main[0].innerHTML = productsHTML
